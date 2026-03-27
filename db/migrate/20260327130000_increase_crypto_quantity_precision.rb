4ea96c1c0847dcd348b0792edaadf73732c03795
Anas Limouri <anas.l@bridgeapi.io>
2026-03-27 13:05:07 +0100

    [BUGFIX] Fix CoinStats migration version

--- db/migrate/20260327130000_increase_crypto_quantity_precision.rb
+++ db/migrate/20260327130000_increase_crypto_quantity_precision.rb
@@ -1,4 +1,4 @@
-class IncreaseCryptoQuantityPrecision < ActiveRecord::Migration[8.0]
+class IncreaseCryptoQuantityPrecision < ActiveRecord::Migration[7.2]
   def change
     change_column :holdings, :qty, :decimal, precision: 24, scale: 8, null: false
     change_column :trades, :qty, :decimal, precision: 24, scale: 8
